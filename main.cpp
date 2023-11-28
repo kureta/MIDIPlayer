e9908d35316bcf913e11d198478ed339674d34a9
kureta <skureta@gmail.com>
2023-11-28 18:58:12 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,68 @@
+#include <raylib.h>
+#include "utils.hpp"
+#include <thread>
+
+
+double t = 0.0;
+double dt = 0.05;
+
+double currentTime = 0.0;
+double accumulator = 0.0;
+
+
+int main()
+{
+    using namespace std::literals;
+
+    // Application window setup
+    SetTraceLogLevel(LOG_ERROR);
+    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+
+    constexpr int screenWidth = 1280;
+    constexpr int screenHeight = 720;
+
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
+    InitWindow(screenWidth, screenHeight, "MIDI Player");
+
+    // MIDI setup
+    libremidi::midi_out midiout;
+    chooseMidiPort(midiout);
+
+    // Main loop
+    currentTime = GetTime();
+    while (!WindowShouldClose()) // Detect window close button or ESC key
+    {
+        // Note On: 144, 64, 90
+        midiout.send_message(144, 64, 90);
+
+        std::this_thread::sleep_for(500ms);
+
+        // Note Off: 128, 64, 40
+        midiout.send_message(128, 64, 40);
+
+        /*
+        const double newTime = GetTime();
+        double frameTime = newTime - currentTime;
+        if (frameTime > 0.25)
+            frameTime = 0.25;
+        currentTime = newTime;
+
+        accumulator += frameTime;
+
+        while (accumulator >= dt)
+        {
+            // do time sensitive stuff
+            accumulator -= dt;
+            t += dt;
+        }
+        */
+
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
---
+++ utils.hpp
@@ -0,0 +1,116 @@
+#pragma once
+
+#include <libremidi/libremidi.hpp>
+
+#include <iostream>
+
+inline std::ostream& operator<<(std::ostream& s, const libremidi::message& message)
+{
+  const unsigned int nBytes = message.size();
+  for (int i = 0U; i < nBytes; i++)
+    s << "Byte " << i << " = " << static_cast<int>(message[i]) << ", ";
+  if (nBytes > 0)
+    s << "stamp = " << message.timestamp;
+  return s;
+}
+
+inline std::ostream& operator<<(std::ostream& s, const libremidi::port_information& rhs)
+{
+  s << "[ client: " << rhs.client << ", port: " << rhs.port;
+  if (!rhs.manufacturer.empty())
+    s << ", manufacturer: " << rhs.manufacturer;
+  if (!rhs.device_name.empty())
+    s << ", device: " << rhs.device_name;
+  if (!rhs.port_name.empty())
+    s << ", portname: " << rhs.port_name;
+  if (!rhs.display_name.empty())
+    s << ", display: " << rhs.display_name;
+  return s << "]";
+}
+
+// This function should be embedded in a try/catch block in case of
+// an exception.  It offers the user a choice of MIDI ports to open.
+// It returns false if there are no ports available.
+inline bool chooseMidiPort(libremidi::midi_in& libremidi)
+{
+  std::cout << "\nWould you like to open a virtual input port? [y/N] ";
+
+  std::string keyHit;
+  std::getline(std::cin, keyHit);
+  if (keyHit == "y")
+  {
+    libremidi.open_virtual_port();
+    return true;
+  }
+
+  const auto ports = libremidi::observer{{}, observer_configuration_for(libremidi.get_current_api())}
+                   .get_input_ports();
+  unsigned int i = 0;
+  const unsigned int nPorts = ports.size();
+  if (nPorts == 0)
+  {
+    std::cout << "No input ports available!" << std::endl;
+    return false;
+  }
+
+  if (nPorts == 1)
+  {
+    std::cout << "\nOpening " << ports[0].display_name << std::endl;
+  }
+  else
+  {
+    for (i = 0; i < nPorts; i++)
+    {
+      std::string portName = ports[i].display_name;
+      std::cout << "  Input port #" << i << ": " << portName << '\n';
+    }
+
+    do
+    {
+      std::cout << "\nChoose a port number: ";
+      std::cin >> i;
+    } while (i >= nPorts);
+  }
+
+  std::cout << "\n";
+  libremidi.open_port(ports[i]);
+
+  return true;
+}
+
+inline bool chooseMidiPort(libremidi::midi_out& libremidi)
+{
+  const auto ports = libremidi::observer{{}, observer_configuration_for(libremidi.get_current_api())}
+                   .get_output_ports();
+  unsigned int i = 0;
+  const unsigned int nPorts = ports.size();
+  if (nPorts == 0)
+  {
+    std::cout << "No output ports available!" << std::endl;
+    return false;
+  }
+
+  if (nPorts == 1)
+  {
+    std::cout << "\nOpening " << ports[0].display_name << std::endl;
+  }
+  else
+  {
+    for (i = 0; i < nPorts; i++)
+    {
+      std::string portName = ports[i].display_name;
+      std::cout << "  Output port #" << i << ": " << portName << '\n';
+    }
+
+    do
+    {
+      std::cout << "\nChoose a port number: ";
+      std::cin >> i;
+    } while (i >= nPorts);
+  }
+
+  std::cout << "\n";
+  libremidi.open_port(ports[i]);
+
+  return true;
+}
